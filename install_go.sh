9738919a4c3c3cdd29efba96b52de0a173045ea8
ballinyouup <bryanhuici@gmail.com>
2026-02-26 00:39:40 -0500

    feat: add permanent PATH update for Go installation in ~/.bashrc

--- install_go.sh
+++ install_go.sh
@@ -10,16 +10,26 @@ sudo rm -rf /usr/local/go
 # Install the new go binary
 sudo tar -C /usr/local -xzf go1.24.0.linux-amd64.tar.gz
 
-# Add go to the PATH
+# Add go to the PATH permanently in ~/.bashrc
+if ! grep -q "/usr/local/go/bin" ~/.bashrc; then
+    echo "" >> ~/.bashrc
+    echo "# Go environment variables" >> ~/.bashrc
+    echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+    echo "Added Go to PATH in ~/.bashrc"
+else
+    echo "Go PATH already exists in ~/.bashrc"
+fi
+
+# Export for current session
 export PATH=$PATH:/usr/local/go/bin
 
 # Verify the installation
 go version
 
 # Clean up
-tar -xzf go1.24.0.linux-amd64.tar.gz
+rm -f go1.24.0.linux-amd64.tar.gz
 
 # Reload shell
 source ~/.bashrc
 
-echo "Go installed successfully"
+echo "Go installed successfully and added to ~/.bashrc"
